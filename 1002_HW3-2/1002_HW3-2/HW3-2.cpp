8de721fc859372013a01d9d4f389a066326dd696
Kim Jisu <xoxo1795@sookmyung.ac.kr>
2022-10-04 15:26:20 +0900

    [Fix] 1004_HW3-2, 3-3
    풀이 추가

--- 1002_HW3-2/1002_HW3-2/HW3-2.cpp
+++ 1002_HW3-2/1002_HW3-2/HW3-2.cpp
@@ -35,3 +35,47 @@ int solution(string my_string) {
 
     return answer;
 }
+
+
+
+
+int changeToDec(vector<int> temp) {
+    int k = 1;
+    int num = 0;
+    for (int i = temp.size() - 1; i >= 0; i--, k *= 10)
+        num += temp[i] * k;
+    return num;
+}
+
+
+
+int getNumbers(string my_string) {
+    vector<int> numbers;
+    vector<int> temp;
+    for (int i = 0; i < my_string.length(); i++) {
+
+        if (my_string[i] < '0' || my_string[i] > '9') {
+            if (temp.size() > 0) {
+                numbers.push_back(changeToDec(temp));
+                temp.clear();
+            }
+        }
+        else {
+            temp.push_back(my_string[i] - '0');
+        }
+
+        numbers.push_back(changeToDec(temp));
+    } return numbers;
+}
+
+
+int solution(string my_string) {
+    int answer = 0;
+
+    vector<int> numbers = getNumbers(my_string);
+    for (int i = 0; i < numbers.size(); i++) {
+        answer += numbers[i];
+    }
+
+    return answer;
+}
--- 1002_HW3-3/1002_HW3-3/HW3-3.cpp
+++ 1002_HW3-3/1002_HW3-3/HW3-3.cpp
@@ -30,3 +30,35 @@ vector<int> solution(vector<string> keyinput, vector<int> board) {
 
     return answer;
 }
+
+
+vector<int> move(vector<int> answer, string key, vector<int> board) {
+
+    int boardX = board[0] / 2, boardY = board[1] / 2;
+    if (key == "left") {
+        if (answer[0] > -boardX) answer[0]--;
+    }
+    else if (key == "right") {
+        if (answer[0] < boardX) answer[0]++;
+    }
+    else if (key == "up") {
+        if (answer[1] < boardY) answer[1]++;
+    }
+    else if (key == "down") {
+        if (answer[1] > -boardY) answer[1]--;
+    }
+}
+
+
+vector<int> solution(vector<string> keyinput, vector<int> board) {
+    vector<int> answer;
+
+    answer.push_back(0);
+    answer.push_back(0);
+
+    for (int i = 0; i < keyinput.size(); i++) {
+        answer = move(answer, keyinput[i], board);
+    }
+
+    return answer;
+}
