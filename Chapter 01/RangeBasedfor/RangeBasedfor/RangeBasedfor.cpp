27aa102648aa1b99604aae0eeb0bb172bf090e1e
Kim Jisu <xoxo1795@sookmyung.ac.kr>
2022-10-05 20:13:21 +0900

    [Feat] 1005_이것이 Cpp_Ch1.7

---
+++ Chapter 01/RangeBasedfor/RangeBasedfor/RangeBasedfor.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <tchar.h>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[]) {
+	int aList[5] = { 10, 20, 30, 40, 50 };
+
+	for (int i = 0; i < 5; ++i) {
+		cout << aList[i] << ' ';
+	} cout << endl;
+
+	// 범위 기반 C++11 스타일 반복문
+	// 각 요소의 값을 n에 복사한다.
+	for (auto n : aList) {
+		cout << n << ' ';
+	} cout << endl;
+
+	// n은 각 요소에 대한 참조다.
+	for (auto& n : aList) {
+		cout << n << ' ';
+	} cout << endl;
+
+	return 0;
+}
