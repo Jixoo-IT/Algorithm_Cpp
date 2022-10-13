931cd90f6074b528a24d1339bda3e77f1812ccdd
Kim Jisu <xoxo1795@sookmyung.ac.kr>
2022-10-13 18:09:24 +0900

    [Fix] Q9, 10

--- C++_Lecture/0929_Q10/0929_Q10/Q10.cpp
+++ C++_Lecture/0929_Q10/0929_Q10/Q10.cpp
@@ -1,27 +1,33 @@
-// Q10. 로그인 성공
+//Q10_로그인 성공(?)
 
+// 풀이
 #include <string>
 #include <vector>
 
 using namespace std;
 
-
 int match(vector<string> id_pw, vector<string> candidate) {
-    if (id_pw[0] == candidate[0] && id_pw[1] == candidate[1]) return 2;
-    else if (id_pw[0] == candidate[0]) return 1;
-    else return 0;
+    if (id_pw[0] == candidate[0] && id_pw[1] == candidate[1]) {
+        return 2;       // login
+    }
+    else if (id_pw[0] == candidate[0]) {
+        return 1;       // wrong pw
+    }
+    else {
+        return 0;       // fail
+    }
 }
-
 
 string solution(vector<string> id_pw, vector<vector<string>> db) {
     string answer = "fail";
 
-    int cat;
+    int result;
     for (int i = 0; i < db.size(); i++) {
-        cat = match(id_pw, db[i]);
-        if (cat == 2)
-            return "login";
-        else if (cat == 1) {
+        result = match(id_pw, db[i]);
+        if (result == 2) {
+            answer = "login";
+        }
+        else if (result == 1) {
             answer = "wrong pw";
         }
     }
--- C++_Lecture/0929_Q9/0929_Q9/Q9.cpp
+++ C++_Lecture/0929_Q9/0929_Q9/Q9.cpp
@@ -1,15 +1,35 @@
-// 가까운 수
+// Q9_가까운 수
+
+// 풀이
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
+int solution(vector<int> array, int n) {
+    int answer = 0;
+    int diff = 100000;
+
+    sort(array.begin(), array.end());       // sort 함수: 배열을 오름차순으로 정렬
+    for (const auto v : array) {         // 범위 기반 for문: for(type element : element_array)
+                                        // 인덱스 0부터 끝까지 해당하는 요소가 element_array에서 element(임시객체)로 복사된다
+        if (diff > abs(v - n)) {       // abs 함수: 절댓값을 반환, array의 원소와 n의 차이가 작은 것을 diff로 함.
+            diff = abs(v - n);
+            answer = v;
+        }
+
+    }
+    return answer;
+}
+
+// 답안
 int getDiff(int n, int m) {
     if (n > m) return n - m;
     else return m - n;
 }
 int solution(vector<int> array, int n) {
-    int answer = -200; //절대 닶이 될수 없는 값으로 초기화
+    int answer = -200; //절대 답이 될수 없는 값으로 초기화
     for (int i = 0; i < array.size(); i++)
         if (getDiff(array[i], n) < getDiff(answer, n) || getDiff(array[i], n) == getDiff(answer, n) && answer > array[i])//같을 때만 해당
             answer = array[i];
