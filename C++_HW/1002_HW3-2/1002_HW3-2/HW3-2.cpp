d3f40ca21fa21a8cc66e5f83dd4de5c33fad3740
Kim Jisu <xoxo1795@sookmyung.ac.kr>
2022-10-14 21:15:45 +0900

    [Fix] HW3-2

--- C++_HW/1002_HW3-2/1002_HW3-2/HW3-2.cpp
+++ C++_HW/1002_HW3-2/1002_HW3-2/HW3-2.cpp
@@ -3,6 +3,7 @@
 // my_string은 소문자, 대문자, 자연수로만 구성되어있습니다. 
 // my_string안의 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
 
+// 풀이
 #include <string>
 #include <vector>
 
@@ -11,22 +12,21 @@ using namespace std;
 int Sum(string a_string) {
     int i, num = 0, sum = 0;
 
-    for (i = 0; a_string[i]; i++) {
-        if (a_string[i] >= '0' && a_string[i] <= '9') {
-            num = num * 10 + a_string[i] - '0';
-        }
-        else {
-            if (num > 0) {
-                sum = sum + num;
-                num = 0;
+    for (i = 0; a_string[i]; i++) { // a_string[i] 값이 없다면 종료
+        if (a_string[i] >= '0' && a_string[i] <= '9') { // 숫자일 때,
+            num = num * 10 + a_string[i] - '0';     // -'0': 아스키 값 0을 반환하여 -48이 된다. / 두 자리 수 숫자일 경우를 고려하기 위해, num * 10
+        }                                           // 만약, a_string[0]이 1이라면, num * 10 + 49 - 48 (1의 아스키코드 값 49)
+        else {                      // 알파벳일 때,
+            if (num > 0) {          // num이 0 이상인 경우(직전에 숫자가 있었을 경우)
+                sum = sum + num;    // sum에 num을 더한 후,
+                num = 0;            // num = 0으로 값 초기화
             }
         }
     }
-    if (num > 0) sum += num;
+    if (num > 0) sum += num;        // 배열의 마지막이 숫자인 경우, num이 더해지지 않고 for문을 빠져나오므로, num을 추가로 더함.
 
     return sum;
 }
-
 
 int solution(string my_string) {
     int answer = 0;
@@ -37,8 +37,7 @@ int solution(string my_string) {
 }
 
 
-
-
+// 답안
 int changeToDec(vector<int> temp) {
     int k = 1;
     int num = 0;
