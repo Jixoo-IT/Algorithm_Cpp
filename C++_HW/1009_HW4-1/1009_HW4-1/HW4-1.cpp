cbfcb8ca1c0141fb6b65731e2e9f4c645861dde4
Kim Jisu <xoxo1795@sookmyung.ac.kr>
2022-10-14 23:17:34 +0900

    [Fix] HW4-1

--- C++_HW/1009_HW4-1/1009_HW4-1/HW4-1.cpp
+++ C++_HW/1009_HW4-1/1009_HW4-1/HW4-1.cpp
@@ -1,26 +1,26 @@
 #include <string>
 #include <vector>
-#include <sstream>
+#include <sstream>      // streamstring을 사용하기 위한 헤더
 
 using namespace std;
 
 string solution(string polynomial) {
     string answer = "";
-    stringstream ss(polynomial);
-    ss.str(polynomial);
+    stringstream ss(polynomial);        // streamstring: 문자열에서 원하는 자료형의 데이터를 추출할 때 사용
+    ss.str(polynomial);                 // str(string s): 현재 stringstream의 값을 s로 바꾼다.
 
     int num_x = 0, num = 0;
-    string token;
+    string token;       // token의 자료형: string
 
-    while (ss >> token) {
+    while (ss >> token) {       // 문자열 ss에서 token의 자료형(string)에 맞는 정보가 없을 때까지 ss에서 token으로 자료를 추출/복사하는 것. 즉, 숫자를 제외한 x만 추출
         if (token != "+") {
-            if (token.find("x") != -1) {
+            if (token.find("x") != -1) {        // string.find("")해당 문자의 시작 인덱스 반환
                 if (token == "x")
                     num_x += 1;
-                else num_x += stoi(token.substr(0, token.size() - 1));
+                else num_x += stoi(token.substr(0, token.size() - 1));      //substr(첫 번째 문자의 위치, 부분 문자열의 길이)
             }
             else {
-                num += stoi(token);
+                num += stoi(token);         // stoi: string to int
             }
         }
     }
