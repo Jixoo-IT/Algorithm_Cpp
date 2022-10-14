7756fb6bdea46dc2953665d066b90a02508f3c20
Kim Jisu <xoxo1795@sookmyung.ac.kr>
2022-10-14 21:08:42 +0900

    [Fix] HW3-1

--- C++_HW/1002_HW3-1/1002_HW3-1/HW3-1.cpp
+++ C++_HW/1002_HW3-1/1002_HW3-1/HW3-1.cpp
@@ -1,17 +1,22 @@
+// HW3-1
+
+// 풀이
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int solution(vector<vector<int>> dots) {
-    int minX = min(min(dots[0][0], dots[1][0]), min(dots[2][0], dots[3][0]));
+    int minX = min(min(dots[0][0], dots[1][0]), min(dots[2][0], dots[3][0]));   // min(a, b), a와 b 중 작은 값을 반환
     int minY = min(min(dots[0][1], dots[1][1]), min(dots[2][1], dots[3][1]));
-    int maxX = max(max(dots[0][0], dots[1][0]), max(dots[2][0], dots[3][0]));
+    int maxX = max(max(dots[0][0], dots[1][0]), max(dots[2][0], dots[3][0]));   // max(a, b), a와 b 중 큰 값을 반환
     int maxY = max(max(dots[0][1], dots[1][1]), max(dots[2][1], dots[3][1]));
     return (maxY - minY) * (maxX - minX);
 }
 
 
+// 답안
 int getDiff(vector<vector<int>> dots, int dim) {
     int max= -256, min= 256;    // 절대 될 수 없는 값으로 초기화
     for (int i = 0; i < dots.size(); i++) {
@@ -20,7 +25,6 @@ int getDiff(vector<vector<int>> dots, int dim) {
     }
     return max - min;
 }
-
 
 int solution(vector<vector<int>> dots) {
     int answer = 0;
