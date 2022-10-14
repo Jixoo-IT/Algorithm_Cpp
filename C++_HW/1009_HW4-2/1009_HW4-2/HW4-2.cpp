82fb4d109cc5997962cd7fa27cac75c718572ded
Kim Jisu <xoxo1795@sookmyung.ac.kr>
2022-10-14 21:46:18 +0900

    [Fix] HW 4-2

--- C++_HW/1009_HW4-2/1009_HW4-2/HW4-2.cpp
+++ C++_HW/1009_HW4-2/1009_HW4-2/HW4-2.cpp
@@ -3,6 +3,25 @@
 
 using namespace std;
 
+// 답안
+
+vector<int> solution(int n) {
+    vector<int> answer;
+
+    for (int i = 2; i <= n; i++) {
+        if (n % i == 0) {
+            answer.push_back(i);
+            while (n % i == 0) { // 12를 소인수분해하면 2*2*3, 2와 같이 중복되는 값을 고려하기 위해, while문을 이용
+                n /= i;
+            }
+        }
+    }
+
+    return answer;
+}
+
+
+// 풀이 (일부 테스트케이스 실패)
 vector<int> solution(int n) {
     vector<int> answer;
 
@@ -18,22 +37,6 @@ vector<int> solution(int n) {
         }
         else {
             i++;
-        }
-    }
-
-    return answer;
-}
-
-// 답안
-
-vector<int> solution(int n) {
-    vector<int> answer;
-
-    for (int i = 2; i <= n; i++) {
-        if (n % i == 0) {
-            answer.push_back(i);
-            while (n % i == 0)
-                n = n / i;
         }
     }
 
