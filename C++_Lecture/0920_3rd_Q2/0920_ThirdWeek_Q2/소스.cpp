0c7a1cf8e59288f094ddf258dcb47d495db97e37
Kim Jisu <xoxo1795@sookmyung.ac.kr>
2022-10-24 21:04:14 +0900

    [Fix] Q2_순열 검사

--- C++_Lecture/0920_3rd_Q2/0920_ThirdWeek_Q2/소스.cpp
+++ C++_Lecture/0920_3rd_Q2/0920_ThirdWeek_Q2/소스.cpp
@@ -6,27 +6,15 @@ using namespace std;
 bool solution(vector<int> arr)
 {
     bool answer = true;
+    int len = arr.size();       // 중복 제거 전에 arr.size()를 저장해야 함.
 
-    sort(arr.begin(), arr.end());
+    arr.erase(unique(arr.begin(), arr.end()), arr.end());   // arr 배열 내, 중복된 원소 제거, 괄호 주의
+    sort(arr.begin(), arr.end());       // 중복 제거된 배열을 오름차순으로 정렬
 
-    for (int i = 0; i < arr.size(); i++) {
-        if (arr[i] != i + 1) {
-            return false;
-        }
+    if (arr.back() != len) {        // 정렬된 arr의 마지막 원소가 그 배열의 길이 값과 다르다면, false
+        answer = false;
     }
 
     return answer;
 
-
-
-/*
-*   vector<int> count;
-    for (int i = 0; i < arr.size(); i++)
-        count.push_back(0);
-    for(int i=0; i < arr.size(); i++){
-        if(arr[i] != i+1){
-            return false;
-        }
-    }
-*/ 
 }
