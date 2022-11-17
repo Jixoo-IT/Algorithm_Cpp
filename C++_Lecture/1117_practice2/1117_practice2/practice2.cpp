aade9065f519a0d018aeab7589a57b64abbce0ea
Kim Jisu <xoxo1795@sookmyung.ac.kr>
2022-11-17 15:22:45 +0900

    [Fix] 1117_Practice2

--- C++_Lecture/1117_practice2/1117_practice2/practice2.cpp
+++ C++_Lecture/1117_practice2/1117_practice2/practice2.cpp
@@ -9,8 +9,9 @@ using namespace std;
 
 class Point {
 private:
-	float x, y;
+	
 public:
+	float x, y;
 	Point();
 	Point(float x, float y);
 	float length();
@@ -41,10 +42,49 @@ ostream& operator<<(ostream& os, const Point& b) {
 	return os;
 }
 
+class ThreePoint : public Point {
+public:
+	float z;
+	ThreePoint();
+	ThreePoint(float x, float y, float z);
+	float length();
+	float operator*(ThreePoint& b);
+	friend ostream& operator<<(ostream& os, const Point& c);
+};
+
+ThreePoint::ThreePoint() {
+	z = 0;
+}
+
+ThreePoint::ThreePoint(float x, float y, float z) : Point(x, y) {
+	this->z = z;
+}
+
+float ThreePoint::length() {
+	return sqrt(Point::length() * Point::length()+ z*z);
+}
+
+float ThreePoint:: operator*(ThreePoint& b) {
+	return x * b.x + y * b.y + z * b.z;
+}
+
+ostream& operator<<(ostream& os, const ThreePoint& b) {
+	os << "(" << b.x << "," << b.y << b.z << ")";
+	return os;
+}
+
 int main() {
+	
 	Point a(2, 1);
 	Point b(3, -4);
 	cout << b << endl;
 	cout << b.length() << endl;
 	cout << (a * b) << endl;
+
+
+	ThreePoint ta(2, 5, 1);
+	ThreePoint tb(3, -4, 5);
+	cout << tb << endl;
+	cout << tb.length() << endl;
+	cout << (ta * tb) << endl;
 }
