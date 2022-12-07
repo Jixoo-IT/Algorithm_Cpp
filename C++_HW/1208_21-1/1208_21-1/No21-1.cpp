73ea2080e88b772a4b2433501531f3a6153e3adf
Kim Jisu <xoxo1795@sookmyung.ac.kr>
2022-12-07 23:54:53 +0900

    [Fix] 21-#1 풀이 완료

--- C++_HW/1208_21-1/1208_21-1/No21-1.cpp
+++ C++_HW/1208_21-1/1208_21-1/No21-1.cpp
@@ -8,58 +8,45 @@ using namespace std;
 
 class Mirror {
 private:
-	double number;
+	int number;
 	string str;
 	string beforestr;
 	string afterstr;
 	string result;
 	bool answer;
 public:
-	Mirror() { number = 0; str = ""; beforestr = ""; afterstr = ""; result = "false"; answer = false; }
-	int isMirrorNum(double num);
-	string print_Num();
+	Mirror() { number = 0; str = ""; beforestr = ""; afterstr = ""; result = ""; answer = false; }
+	string isMirrorNum(int num);
 };
 
-int Mirror::isMirrorNum(double num) {
+string Mirror::isMirrorNum(int num) {
 	number = num;
 	str = to_string(number);
 	beforestr = str;
+	cout << beforestr << endl;
 
 	reverse(str.begin(), str.end());
 	afterstr = str;
+	cout << afterstr << endl;
 
 	if (beforestr == afterstr) {
 		answer = true;
+		result = "true";
 	}
 	else {
 		answer= false;
-	}
-	return answer;
-}
-
-string Mirror::print_Num() {
-	if (answer = true) {
-		result = "true";
-	}
-	else if(answer = false) {
 		result = "false";
 	}
-	cout << result << endl;
-
+	
 	return result;
 }
 
 int main() {
 	Mirror m;
-
 	cout << m.isMirrorNum(767) << endl;
-	m.print_Num();
 	cout << m.isMirrorNum(123321) << endl;
-	m.print_Num();
 	cout << m.isMirrorNum(121212) << endl;
-	m.print_Num();
 	cout << m.isMirrorNum(7575) << endl;
-	m.print_Num();
 
 	return 0;
 }
