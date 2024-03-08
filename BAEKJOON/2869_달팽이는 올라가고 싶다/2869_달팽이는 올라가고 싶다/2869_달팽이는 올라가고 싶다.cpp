4c7c4f0b75b7acd778f441afe2ae350e1d4f1126
Kim Jisu <xoxo1795@sookmyung.ac.kr>
2024-03-08 18:25:47 +0900

    [Fix] BJ- 달팽이는 올라가고 싶다

--- BAEKJOON/2869_달팽이는 올라가고 싶다/2869_달팽이는 올라가고 싶다/2869_달팽이는 올라가고 싶다.cpp
+++ BAEKJOON/2869_달팽이는 올라가고 싶다/2869_달팽이는 올라가고 싶다/2869_달팽이는 올라가고 싶다.cpp
@@ -1,34 +1,68 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
+int main() {
+	int A, B, V, height = 0, day = 0;
+	cin >> A >> B >> V;
+
+	if (A >= V) {
+		cout << "1";
+	} 
+	else if ((V - A) % (A - B) == 0) {	// 나머지 == 0이라면, 딱 A만큼 더 가면 V에 도달하므로 +1을 해준다.
+		cout << ((V - A) / (A - B) + 1);
+	}
+	else if ((V - A) % (A - B) != 0) {		// 나머지 != 0이라면, (A-B)만큼 한 번더 올라간 후 A보다 작은 높이가 남은 것이므로 +2
+		cout << ((V - A) / (A - B) + 2);
+	}
+
+
+	return 0;
+}
+
+
+/* 오류 발생
+int main() {
+	float A, B, V, height = 0, day = 0;
+	cin >> A >> B >> V;
+	
+	if (A >= V) {
+		cout << "1";
+	}
+	cout << fixed;
+	cout.precision(0);
+
+	day = ceil((V-B)/(A-B));
+	cout << day;
+}
+*/
+
+/* 또 시간초과 / 하루에 갈 수 있는 거리를 A-B미터로 두면 달팽이가 정상에 도달했는지 여부와 상관없이 미끄러졌다고 가정해야 함,,
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int A, B, V, height = 0, day = 0, before;
+	int A, B, V, height = 0, day = 0, before, semibefore;
 	cin >> A >> B >> V;
 
 	before = A - 2 * B;
+	semibefore = A - B;
 	while (1) {
 		height += (A - B);
 		day++;
+		cout << day << endl;
 		if (height >= V) {
-			cout << "hi";
+			height -= before;
+			day--;
+			cout << day << "   ";
 			while (1) {
-				height -= before;
 				if (height <= V) {
 					break;
 				}
+				height -= semibefore;
 				day--;
-				if ((height - before) >= V) {
-					height = height - before;
-					cout << "bye" << height << endl;
-					day--;
-				}
-				else {
-					break;
-				}
+				cout << day << "   ";
 			}
 			break;
 		}
@@ -39,6 +73,7 @@ int main() {
 	return 0;
 }
 
+*/
 
 /* 시간초과 발생
 int main() {
