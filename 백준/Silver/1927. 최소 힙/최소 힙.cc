8b8b27cd83a4d78d291d138e7c494cbb5dc9386d
KimJisu-IT <86948824+KimJisu-IT@users.noreply.github.com>
2024-03-26 11:34:29 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n, num;
+	priority_queue<int, vector<int>, greater<int>> pq;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> num;
+
+		if (pq.empty() && num == 0) {
+			cout << "0" << "\n";
+		}
+		else if (num == 0) {
+			cout << pq.top() << "\n";	// pq.top(): 우선순위가 높은 원소 반환, q.front(): 큐 제일 앞에 있는 원소를 반환
+			pq.pop();
+		}
+		else {
+			pq.push(num);
+		}
+	}
+
+	return 0;
+}
