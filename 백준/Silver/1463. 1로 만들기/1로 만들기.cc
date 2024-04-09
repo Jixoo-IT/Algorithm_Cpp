2d7368eaa1fb407f41e0780beb288b95aaf64e9c
KimJisu-IT <86948824+KimJisu-IT@users.noreply.github.com>
2024-04-09 11:20:28 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	vector<int> dp(n + 1);
+
+	// bottom-up
+	dp[1] = 0;
+	
+	for (int i = 2; i <= n; i++) {
+		dp[i] = dp[i - 1] + 1;		// -1 연산을 하는 경우
+
+		if (i % 2 == 0) {
+			dp[i] = min(dp[i], dp[i / 2] + 1);		// 나누기 2 연산을 하는 경우 | min 함수를 이용해 연산의 최솟값만 저장함.
+		}
+		
+		if (i % 3 == 0) {
+			dp[i] = min(dp[i], dp[i / 3] + 1);		// 나누기 3 연산을 하는 경우
+		}
+	}
+	cout << dp[n];
+
+	return 0;
+}
