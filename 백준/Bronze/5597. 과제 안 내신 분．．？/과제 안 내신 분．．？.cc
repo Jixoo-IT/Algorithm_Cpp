6b162a17bd0638fe7770d069c464f53319029f25
KimJisu-IT <86948824+KimJisu-IT@users.noreply.github.com>
2024-07-22 19:04:53 +0800

    [Bronze III] Title: 과제 안 내신 분..?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+int main() {
+	int arr[31] = { 0, };	// array를 모두 0으로 초기화
+	//fill_n(arr, 31, -1)	// 만약, 0이 아닌 다른 숫자로 모두 초기화하고 싶다면, fill_n 함수를 사용하자.
+	int num, answer;
+
+	for (int i = 0; i < 28; i++) {
+		cin >> num;
+		arr[num] = 1;
+	}
+
+	for (int i = 1; i <= 30; i++) {
+		if (arr[i] == 0) {
+			cout << i << "\n";
+		}
+	}
+
+	return 0;
+}
