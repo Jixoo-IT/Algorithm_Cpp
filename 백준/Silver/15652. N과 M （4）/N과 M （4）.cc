54c60d86f1eb0c2d6e5f6ef77b128c446d7a3be2
KimJisu-IT <86948824+KimJisu-IT@users.noreply.github.com>
2024-08-19 17:53:53 +0900

    [Silver III] Title: N과 M (4), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int n, m;
+int arr[9] = { 0, };
+
+void dfs(int num, int k) {
+    if (k == m) { // M에 도달했다면,
+        for (int i = 0; i < m; i++)
+            cout << arr[i] << " ";
+        cout << "\n";
+    }
+    else {
+        for (int i = num; i <= n; i++) { // 1부터 n까지 반복문
+            arr[k] = i; // 값 저장
+            dfs(i, k + 1); // m에 도달할 때까지 재귀 반복
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> n >> m;
+
+    dfs(1, 0);
+}
