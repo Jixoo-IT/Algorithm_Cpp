196b8c34ba0fac767c9ca18e68dce1d522879b5e
Kim Jisoo <86948824+Jixoo-IT@users.noreply.github.com>
2026-01-21 17:03:49 +0900

    [Bronze V] Title: UOS 문자열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32929. UOS 문자열/UOS 문자열.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int x;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> x;
+
+    if (x % 3 == 0){
+        cout << "S";
+    } else if (x % 3 == 1){
+        cout << "U";
+    } else if (x % 3 == 2){
+        cout << "O";
+    }
+
+    return 0;
+}
