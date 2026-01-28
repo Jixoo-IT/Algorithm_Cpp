9d7a9864dd0dc2fd11238a008a4e0007a273b98b
Kim Jisoo <86948824+Jixoo-IT@users.noreply.github.com>
2026-01-28 20:23:53 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int a1, a0, c, n;
+    cin >> a1 >> a0 >> c >> n;
+
+    if (a1 * n + a0 <= c * n && a1 <= c){
+        cout << "1";
+    } else {
+        cout << "0";
+    }
+    return 0;
+}
