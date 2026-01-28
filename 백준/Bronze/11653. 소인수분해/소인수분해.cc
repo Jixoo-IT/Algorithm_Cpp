7688311ca1097cf1352bdf5511864bd472930dce
Kim Jisoo <86948824+Jixoo-IT@users.noreply.github.com>
2026-01-28 23:58:02 +0900

    [Bronze I] Title: 소인수분해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+vector<int> v;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+
+    if (n == 1) return 0;
+
+    for (int i=2; i*i<=n; i++){
+        while (n % i == 0){
+            cout << i << "\n";
+            n /= i;
+        }
+    }
+
+    if (n > 1){
+        cout << n << "\n";
+    }
+
+    return 0;
+}
