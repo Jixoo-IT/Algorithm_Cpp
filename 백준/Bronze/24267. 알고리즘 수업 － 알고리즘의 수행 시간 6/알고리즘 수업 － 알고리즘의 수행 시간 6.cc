df7a2b0e7f7e9e54ff131c36a7a16a7a249a2cbb
Kim Jisoo <86948824+Jixoo-IT@users.noreply.github.com>
2026-01-28 19:55:40 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long n, ans;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    
+    cout << n*(n-1)*(n-2)/6 << "\n" << "3";    
+    return 0;
+}
