079ded3a09c09c4f948ddf91ea847ccafefdc195
Kim Jisoo <86948824+Jixoo-IT@users.noreply.github.com>
2026-01-28 20:38:59 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a, b;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    while (1){
+        cin >> a >> b;
+
+        if (a == 0 && b == 0) break;
+
+        if (b % a == 0){
+            cout << "factor" << "\n";
+        } else if (a % b == 0){
+            cout << "multiple" << "\n";
+        } else {
+            cout << "neither" << "\n";
+        }
+    }
+    return 0;
+}
