21a3be9b505426737e471f34c3c8b94dbe43dc23
Kim Jisoo <86948824+Jixoo-IT@users.noreply.github.com>
2026-02-04 16:08:58 +0900

    [Bronze I] Title: 스캐너, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/3035. 스캐너/스캐너.cc
+++ 백준/Bronze/3035. 스캐너/스캐너.cc
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-char arr[51][51];
+char ch;
 int r, c, zr, zc;
-vector<char> v;
 
 int main(){
     ios::sync_with_stdio(0);
@@ -10,21 +9,18 @@ int main(){
     cin >> r >> c >> zr >> zc;
 
     for (int i=0; i<r; i++){
-        v.clear();
+        string s;
         for (int j=0; j<c; j++){
-            cin >> arr[i][j];
+            cin >> ch;
 
             for (int k=0; k<zc; k++){
-                cout << arr[i][j];
-                v.push_back(arr[i][j]);
+                cout << ch;
+                s += ch;
             }
         }
         cout << "\n";
         for (int l=0; l<zr-1; l++){
-            for (auto a: v){
-                cout << a;
-            }
-            cout << "\n";
+            cout << s << "\n";
         }
     }
 
