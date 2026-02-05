0826bfc39d1201bcef25553061a1a2d2690f58bb
Kim Jisoo <86948824+Jixoo-IT@users.noreply.github.com>
2026-02-06 02:27:22 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -1,34 +1,28 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-
 int n, m;
-int arr[9] = { 0, };
-bool visited[9] = { 0, };    // 중복 X 위해 사용
+int arr[10];
 
-void dfs(int num, int k) {
-    if (k == m) { // M에 도달했다면,
-        for (int i = 0; i < m; i++)
+void dfs(int num, int idx){
+    if (idx == m){
+        for (int i=0; i<m; i++){
             cout << arr[i] << " ";
+        }
         cout << "\n";
-        
         return;
     }
-    
-    for (int i = num; i <= n; i++) {
-        if (!visited[i]) {
-            visited[i] = true;
-            arr[k] = i; // 값 저장
-            dfs(i + 1, k + 1); // m에 도달할 때까지 재귀 반복
-            visited[i] = false;
-        }
+
+    for (int i=num; i<=n; i++){
+        arr[idx] = i;
+        dfs(i+1, idx+1);
     }
 }
 
-int main() {
-    ios_base::sync_with_stdio(false);
+int main(){
+    ios::sync_with_stdio(0);
     cin.tie(0);
-
     cin >> n >> m;
 
     dfs(1, 0);
+    return 0;
 }
