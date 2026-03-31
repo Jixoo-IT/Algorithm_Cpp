a28e083c2488a92139df9b36e2de09d0bbce7c7a
Kim Jisoo <86948824+Jixoo-IT@users.noreply.github.com>
2026-03-31 20:19:49 +0900

    [Gold V] Title: 토마토, Time: 76 ms, Memory: 10240 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,64 +1,64 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <queue>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
-
+int dy[4] = {-1, 0, 1, 0};
+int dx[4] = {0, -1, 0, 1};
+int m, n, mx = -1e9, arr[1004][1004];
+bool flag;
 queue<pair<int, int>> q;
 
-int dx[4] = { -1, 1, 0, 0 };
-int dy[4] = { 0, 0, -1, 1 };
-
-int n, m, result = 0;
-int tomato[1001][1001];
-
-void bfs(void) {
-    while (!q.empty()) {
-        int x = q.front().first;
-        int y = q.front().second;
-        q.pop();
-
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            // 지도를 벗어나지 않고, 익지않았다면(0)
-            if (nx >= 0 && nx < n && ny >= 0 && ny < m && tomato[nx][ny] == 0) {
-                tomato[nx][ny] = tomato[x][y] + 1;
-                q.push({ nx, ny });
-            }
-        }
-    }
-}
-
-int main() {
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     cin >> m >> n;
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            scanf("%d", &tomato[i][j]);
+    for (int i=0; i<n; i++){
+        for (int j=0; j<m; j++){
+            cin >> arr[i][j];
 
-            if (tomato[i][j] == 1) {
-                q.push({ i, j });
+            if (arr[i][j] == 0) flag = 1;
+        }
+    }
+
+    if (flag == 0) {
+        cout << flag;
+        return 0;
+    }
+
+    for (int i=0; i<n; i++){
+        for (int j=0; j<m; j++){
+            if (arr[i][j] == 1){
+                q.push({i, j});
             }
         }
     }
 
-    bfs();
+    while (q.size()){
+        auto[y, x] = q.front();
+        q.pop();
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+        for (int i=0; i<4; i++){
+            int ny = y + dy[i];
+            int nx = x + dx[i];
 
-            if (tomato[i][j] == 0) {
-                cout << "-1" << "\n";
+            if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+            if (arr[ny][nx] == 0){
+                arr[ny][nx] = arr[y][x] + 1;
+                q.push({ny, nx});
+
+                mx = max(arr[ny][nx], mx);
+            }
+        }
+    }
+
+    for (int i=0; i<n; i++){
+        for (int j=0; j<m; j++){
+            if (arr[i][j] == 0){
+                cout << -1;
                 return 0;
             }
-            if (result < tomato[i][j]) {
-                result = tomato[i][j];
-            }
         }
     }
 
-    cout << result - 1 << "\n";
+    cout << mx - 1;
     return 0;
 }
